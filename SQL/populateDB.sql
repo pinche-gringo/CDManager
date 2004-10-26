14ead207bd2586050e0fa14b6b50cefdd2df800d
Markus Schwab <g17m0@users.sourceforge.net>
2004-10-26 22:16:55 +0000

    Added undefined for genres; added Skapunk-genre

--- SQL/populateDB.sql
+++ SQL/populateDB.sql
@@ -1,6 +1,7 @@
 USE CDMedia
 
 DELETE FROM MGenres;
+INSERT INTO MGenres VALUES (0, 'Undefined');
 INSERT INTO MGenres VALUES (1, 'Comedy');
 INSERT INTO MGenres VALUES (2, 'Drama');
 INSERT INTO MGenres VALUES (3, 'Musical');
@@ -9,6 +10,7 @@ INSERT INTO MGenres VALUES (5, 'Tragedy');
 INSERT INTO MGenres VALUES (6, 'War');
 
 DELETE FROM Genres;
+INSERT INTO Genres VALUES (0, 'Undefined');
 INSERT INTO Genres VALUES (1, 'Alternative');
 INSERT INTO Genres VALUES (2, 'Classic');
 INSERT INTO Genres VALUES (3, 'Electronic');
@@ -19,4 +21,5 @@ INSERT INTO Genres VALUES (7, 'Latin');
 INSERT INTO Genres VALUES (8, 'Metal');
 INSERT INTO Genres VALUES (9, 'Punk');
 INSERT INTO Genres VALUES (10, 'Ska');
+INSERT INTO Genres VALUES (11, 'Skapunk');
 

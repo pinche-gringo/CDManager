8bf1bf6b19bd5051c555c95b202b10e0d996affd
Markus Schwab <g17m0@users.sourceforge.net>
2004-10-30 14:47:37 +0000

    Fixes

--- src/RecEdit.cpp
+++ src/RecEdit.cpp
@@ -1,11 +1,11 @@
-//$Id: RecEdit.cpp,v 1.5 2004/10/28 19:11:52 markus Exp $
+//$Id: RecEdit.cpp,v 1.6 2004/10/30 14:47:37 markus Rel $
 
 //PROJECT     : CDManager
 //SUBSYSTEM   : RecordEdit
 //REFERENCES  :
 //TODO        :
 //BUGS        :
-//REVISION    : $Revision: 1.5 $
+//REVISION    : $Revision: 1.6 $
 //AUTHOR      : Markus Schwab
 //CREATED     : 17.10.2004
 //COPYRIGHT   : Anticopyright (A) 2004
@@ -55,12 +55,13 @@
 /// (Default-)Constructor
 //-----------------------------------------------------------------------------
 RecordEdit::RecordEdit (HRecord record)
-   : XGP::XDialog (OKCANCEL), pClient (manage (new Gtk::Table (5, 3, false))),
+   : XGP::XDialog (OKCANCEL),
+     hRecord (record),
+     pClient (manage (new Gtk::Table (5, 3, false))),
      txtRecord (NULL),
      optArtist (manage (new Gtk::ComboBox ())),
      optGenre (manage (new Gtk::ComboBox ())),
      lstSongs (manage (new Gtk::TreeView ())),
-     hRecord (record),
      mArtists (Gtk::ListStore::create (colArtists)),
      mGenres (Gtk::ListStore::create (colGenres)),
      mSongs (Gtk::ListStore::create (colSongs)) {
@@ -75,7 +76,7 @@ RecordEdit::RecordEdit (HRecord record)
    Gtk::Label* lblArtist (manage (new Gtk::Label (_("_Artist:"), true)));
    Gtk::Label* lblYear (manage (new Gtk::Label (_("_Year:"), true)));
    Gtk::Label* lblGenre (manage (new Gtk::Label (_("_Genre:"), true)));
-   Gtk::Adjustment* spinYear_adj (manage (new Gtk::Adjustment (2000.0, 1900.0, 3000.0, 1.0, 10.0, 4.0)));
+   Gtk::Adjustment* spinYear_adj (manage (new Gtk::Adjustment (2000.0, 0.0, 3000.0, 1.0, 10.0, 4.0)));
    spinYear = manage (new XGP::XAttributeEntry<unsigned int, Gtk::SpinButton> (hRecord->year));
 
    spinYear->set_adjustment (*spinYear_adj);
@@ -124,19 +125,18 @@ RecordEdit::RecordEdit (HRecord record)
    fillGenres ();
    fillInterprets ();
 
-   if (hRecord.isDefined ()) {
-      txtRecord->set_text (hRecord->name);
+   if (hRecord->year || (hRecord->id))
       spinYear->set_value (hRecord->year);
 
-      Check3 (YGP::RelationManager::getRelation ("songs"));
-      Check3 (typeid (*YGP::RelationManager::getRelation ("songs"))
-	      == typeid (YGP::Relation1_N<HRecord, HSong>));
+   Check3 (YGP::RelationManager::getRelation ("songs"));
+   Check3 (typeid (*YGP::RelationManager::getRelation ("songs"))
+	   == typeid (YGP::Relation1_N<HRecord, HSong>));
 
-      YGP::Relation1_N<HRecord, HSong>* relSongs
-	 (dynamic_cast<YGP::Relation1_N<HRecord, HSong>*>
-	  (YGP::RelationManager::getRelation ("songs")));
-      Check3 (relSongs);
-      Check3 (relSongs->isRelated (hRecord));
+   YGP::Relation1_N<HRecord, HSong>* relSongs
+      (dynamic_cast<YGP::Relation1_N<HRecord, HSong>*>
+       (YGP::RelationManager::getRelation ("songs")));
+   Check3 (relSongs);
+   if (relSongs->isRelated (hRecord)) {
       Check3 (relSongs->getObjects (hRecord).size ());
 
       for (std::vector<HSong>::iterator i (relSongs->getObjects (hRecord).begin ());
@@ -164,22 +164,21 @@ RecordEdit::~RecordEdit () {
 /// Handling of the OK button; closes the dialog with commiting data
 //-----------------------------------------------------------------------------
 void RecordEdit::okEvent () {
-   if (!hRecord.isDefined ()) {
-      hRecord.define ();
-      hRecord->id = 0;
-   }
+   Check (hRecord.isDefined ());
+
+   ok->grab_focus ();
 
    std::stringstream update;
    if (txtRecord->hasChanged ()) {
       txtRecord->commit ();
-      update << "name=\"" << Glib::locale_from_utf8 (hRecord->name) << "\" ";
+      update << "name=\"" << Glib::locale_from_utf8 (hRecord->name) << '"';
    }
 
    if (hRecord->year != (unsigned int)spinYear->get_value_as_int ()) {
       hRecord->year = spinYear->get_value_as_int ();
       if (update.str ().size ())
 	 update << ", ";
-      update << "year=" << hRecord->year << ' ';
+      update << "year=" << hRecord->year;
    }
 
    Check3 (optGenre->get_active ());
@@ -188,7 +187,7 @@ void RecordEdit::okEvent () {
    if (hRecord->genre != genre) {
       if (update.str ().size ())
 	 update << ", ";
-      update << "genre=\"" << (*optGenre->get_active ())[colGenres.colID] << "\" ";
+      update << "genre=\"" << (*optGenre->get_active ())[colGenres.colID] << '"';
    }
 
    YGP::Relation1_N<HInterpret, HRecord>* rel;
@@ -200,10 +199,13 @@ void RecordEdit::okEvent () {
 	  (YGP::RelationManager::getRelation ("records")));
    Check3 (rel);
 
-   HInterpret hArtist (rel->getParent (hRecord));
+   HInterpret hArtist;
+   if (rel->isRelated (hRecord))
+      hArtist = rel->getParent (hRecord);
+   else
+      hArtist.define ();
    Check3 (hArtist.isDefined ());
-   TRACE5 ("RecordEdit::okEvent () - Artist: "
-	   << (hArtist.isDefined () ? hArtist->name.c_str () : "None"));
+   TRACE5 ("RecordEdit::okEvent () - Artist: " << hArtist->name);
 
    Check3 (optArtist->get_active ());
    Check3 (*optArtist->get_active ());
@@ -220,10 +222,10 @@ void RecordEdit::okEvent () {
       try {
 	 std::stringstream query;
 	 if (hRecord->id)
-	    query << "UPDATE Records SET " << update.str () << "WHERE id="
+	    query << "UPDATE Records SET " << update.str () << " WHERE id="
 		  << hRecord->id;
 	 else
-	    query << "INSERT into Records SET " << update;
+	    query << "INSERT into Records SET " << update.str ();
 	 Database::store (query.str ().c_str ());
 
       }
@@ -283,8 +285,11 @@ void RecordEdit::fillInterprets () {
 	     (YGP::RelationManager::getRelation ("records")));
       Check3 (rel);
 
-      hArtist = rel->getParent (hRecord);
-      Check3 (hArtist.isDefined ());
+      if (rel->isRelated (hRecord)) {
+	 hArtist = rel->getParent (hRecord);
+	 Check3 (hArtist.isDefined ());
+	 optArtist->set_sensitive (false);
+      } 
       TRACE5 ("RecordEdit::fillInterprets () - Artist: "
 	      << (hArtist.isDefined () ? hArtist->name.c_str () : "None"));
    }

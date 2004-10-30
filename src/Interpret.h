c21c241b663310047cb3716030572a42a2cc4c36
Markus Schwab <g17m0@users.sourceforge.net>
2004-10-30 17:50:36 +0000

    Added sort-function (by name)

--- src/Interpret.h
+++ src/Interpret.h
@@ -1,7 +1,7 @@
 #ifndef INTERPRET_H
 #define INTERPRET_H
 
-//$Id: Interpret.h,v 1.1 2004/10/22 03:45:23 markus Rel $
+//$Id: Interpret.h,v 1.2 2004/10/30 17:50:36 markus Exp $
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,11 +18,17 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 
+#include <vector>
+
 #include <glibmm/ustring.h>
 
 #include <YGP/ADate.h>
 #include <YGP/Handle.h>
 #include <YGP/Entity.h>
+
+
+class Interpret;
+defineHndl(Interpret);
 
 
 /**Class to hold an interpret
@@ -32,8 +38,10 @@ class Interpret : public YGP::Entity {
    friend class RecordEdit;
 
  public:
-   Interpret () { }
+   Interpret () : id (0) { }
    virtual ~Interpret () { }
+
+   static bool compByName (const HInterpret& a, const HInterpret& b);
 
  private:
    unsigned long int id;
@@ -41,9 +49,13 @@ class Interpret : public YGP::Entity {
    YGP::ADate        born;
    YGP::ADate        died;
 
+   static Glib::ustring removeIgnored (const Glib::ustring& name);
+
    //Prohibited manager functions
    Interpret (const Interpret& other);
    const Interpret& operator= (const Interpret& other);
+
+   static std::vector<Glib::ustring> ignore;
 };
 defineHndl(Interpret);
 

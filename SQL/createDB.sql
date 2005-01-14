f72ec5be64468be6e7697058c1c257b3c0942479
Markus Schwab <g17m0@users.sourceforge.net>
2005-01-14 22:41:49 +0000

    Removing genre-tables; Removed foreign-key constraints

--- SQL/createDB.sql
+++ SQL/createDB.sql
@@ -1,11 +1,5 @@
 USE CDMedia;
 
-DROP TABLE MGenres;
-CREATE TABLE MGenres
-    (id	        BIGINT UNSIGNED NOT NULL,
-     genre      VARCHAR(64)     NOT NULL,
-     PRIMARY KEY (id));
-
 DROP TABLE Celebrities;
 CREATE TABLE Celebrities
     (name       VARCHAR (64)    NOT NULL,
@@ -17,69 +11,53 @@ CREATE INDEX names ON Celebrities (name);
 
 DROP TABLE Directors;
 CREATE TABLE Directors
-    (id	        BIGINT UNSIGNED NOT NULL,
-     PRIMARY KEY (id),
-     FOREIGN KEY (celebrity_id) REFERENCES Celebrities(id) ON DELETE CASCADE);
+    (id	        BIGINT UNSIGNED NOT NULL REFERENCES Celebrities(id),
+     PRIMARY KEY (id));
 
 DROP TABLE Movies;
 CREATE TABLE Movies
     (name       VARCHAR (64)    NOT NULL,
      id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
      year       YEAR            NOT NULL DEFAULT 0,
-     director   BIGINT UNSIGNED NOT NULL DEFAULT 0,
+     director   BIGINT UNSIGNED NOT NULL DEFAULT 0 REFERENCES Directors(id),
      genre      BIGINT UNSIGNED NOT NULL DEFAULT 0,
      type       INT UNSIGNED    NOT NULL DEFAULT 0,
      languages  VARCHAR(14)     NOT NULL,
      subtitles  VARCHAR(17)     NOT NULL,
-     PRIMARY KEY (id),
-     FOREIGN KEY (genre_id) REFERENCES MGenres(id) ON DELETE SET NULL,
-     FOREIGN KEY (director_id) REFERENCES Directors(id) ON DELETE SET NULL);
+     PRIMARY KEY (id));
 CREATE INDEX movieNames ON Movies (name);
 
 DROP TABLE MovieNames;
-CREATE TABLE MoviesNames
-    (id         BIGINT UNSIGNED NOT NULL,
+CREATE TABLE MovieNames
+    (id         BIGINT UNSIGNED NOT NULL REFERENCES Movies(id),
      name       VARCHAR (64)    NOT NULL,
      language   CHAR(2)         NOT NULL,
-     PRIMARY KEY (id, language),
-     FOREIGN KEY (movie_id) REFERENCES Movies(id) ON DELETE CASCADE);
-CREATE INDEX movieTranlations ON MoviesNames (id);
+     PRIMARY KEY (id, language));
+CREATE INDEX movieTranlations ON MovieNames (id);
 
 DROP TABLE Actors;
 CREATE TABLE Actors
-    (id	        BIGINT UNSIGNED NOT NULL,
-     PRIMARY KEY (id),
-     FOREIGN KEY (celebrity_id) REFERENCES Celebrities(id) ON DELETE CASCADE);
+    (id	        BIGINT UNSIGNED NOT NULL REFERENCES Celebrities(id),
+     PRIMARY KEY (id));
 
 DROP TABLE ActorsInMovies;
 CREATE TABLE ActorsInMovies
-    (idActor    BIGINT UNSIGNED NOT NULL,
-     idMovie    BIGINT UNSIGNED NOT NULL,
-     FOREIGN KEY (movie_id) REFERENCES Movies(id) ON DELETE CASCADE,
-     FOREIGN KEY (actor_id) REFERENCES Actors(id) ON DELETE CASCADE);
-
-DROP TABLE Genres;
-CREATE TABLE Genres
-    (id	        BIGINT UNSIGNED NOT NULL,
-     genre      VARCHAR(64)     NOT NULL,
-     PRIMARY KEY (id));
+    (idActor    BIGINT UNSIGNED NOT NULL REFERENCES Celebrities(id),
+     idMovie    BIGINT UNSIGNED NOT NULL REFERENCES Movies(id));
 
 DROP TABLE Interprets;
 CREATE TABLE Interprets
-    (id	        BIGINT UNSIGNED NOT NULL,
-     PRIMARY KEY (id),
-     FOREIGN KEY (celebrity_id) REFERENCES Celebrities(id) ON DELETE CASCADE);
+    (id	        BIGINT UNSIGNED NOT NULL REFERENCES Celebrities(id),
+     PRIMARY KEY (id));
 
 DROP TABLE Records;
 CREATE TABLE Records
     (name       VARCHAR(64)     NOT NULL,
      id	        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
      year       YEAR            NOT NULL DEFAULT 0,
-     interpret  BIGINT UNSIGNED NOT NULL,
+     interpret  BIGINT UNSIGNED NOT NULL REFERENCES Interprets(id),
      genre      int             NOT NULL DEFAULT 0,
-     PRIMARY KEY (id),
-     FOREIGN KEY (genre_id) REFERENCES Genres(id) ON DELETE SET NULL,
-     FOREIGN KEY (interpret_id) REFERENCES Interprets(id) ON DELETE CASCADE);
+     PRIMARY KEY (id));
 CREATE INDEX recordNames ON Records (name);
 CREATE INDEX recordInterprets ON Records (interpret);
 
@@ -88,12 +66,10 @@ CREATE TABLE Songs
     (name       VARCHAR(64)       NOT NULL,
      id	        BIGINT UNSIGNED	  NOT NULL AUTO_INCREMENT,
      track      SMALLINT UNSIGNED NOT NULL,
-     idRecord   BIGINT UNSIGNED   NOT NULL,
+     idRecord   BIGINT UNSIGNED   NOT NULL REFERENCES Records(id),
      duration   TIME              NOT NULL,
      genre      BIGINT UNSIGNED   DEFAULT 0,
-     PRIMARY KEY (id),
-     FOREIGN KEY (genre_id) REFERENCES Genres(id) ON DELETE SET NULL,
-     FOREIGN KEY (interpret_id) REFERENCES Records(id) ON DELETE CASCADE);
+     PRIMARY KEY (id));
 CREATE INDEX songNames ON Songs (name);
 CREATE INDEX songRecord ON Songs (idRecord);
 
@@ -101,3 +77,8 @@ DROP TABLE Words;
 CREATE TABLE Words
     (word       VARCHAR(32)     NOT NULL,
      PRIMARY KEY (word));
+
+DROP TABLE Articles;
+CREATE TABLE Articles
+    (article     VARCHAR(9)    NOT NULL,
+     PRIMARY KEY (article));

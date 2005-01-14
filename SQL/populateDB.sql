3905e3834a0c56955ab2af6cd0bbe5ed1927a2a3
Markus Schwab <g17m0@users.sourceforge.net>
2005-01-14 22:43:33 +0000

    Removed filling genres

--- SQL/populateDB.sql
+++ SQL/populateDB.sql
@@ -1,29 +1,5 @@
 USE CDMedia
 
-DELETE FROM MGenres;
-INSERT INTO MGenres VALUES (0, 'Undefined');
-INSERT INTO MGenres VALUES (1, 'Comedy');
-INSERT INTO MGenres VALUES (2, 'Drama');
-INSERT INTO MGenres VALUES (3, 'Musical');
-INSERT INTO MGenres VALUES (4, 'Satire');
-INSERT INTO MGenres VALUES (5, 'Tragedy');
-INSERT INTO MGenres VALUES (6, 'War');
-INSERT INTO MGenres VALUES (7, 'Documental');
-
-DELETE FROM Genres;
-INSERT INTO Genres VALUES (0, 'Undefined');
-INSERT INTO Genres VALUES (1, 'Alternative');
-INSERT INTO Genres VALUES (2, 'Classic');
-INSERT INTO Genres VALUES (3, 'Electronic');
-INSERT INTO Genres VALUES (4, 'Gothic');
-INSERT INTO Genres VALUES (5, 'Hardcore');
-INSERT INTO Genres VALUES (6, 'Industrial');
-INSERT INTO Genres VALUES (7, 'Latin');
-INSERT INTO Genres VALUES (8, 'Metal');
-INSERT INTO Genres VALUES (9, 'Punk');
-INSERT INTO Genres VALUES (10, 'Ska');
-INSERT INTO Genres VALUES (11, 'Skapunk');
-
 DELETE FROM Words;
 INSERT INTO Words VALUES ('Alan');
 INSERT INTO Words VALUES ('Alain');

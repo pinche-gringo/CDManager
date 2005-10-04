e12812df1252c7b7804178a1a9e1b38a22840105
Markus Schwab <g17m0@users.sourceforge.net>
2005-10-04 16:22:21 +0000

    Added undo

--- src/CDManager.h
+++ src/CDManager.h
@@ -1,7 +1,7 @@
 #ifndef CDMANAGER_H
 #define CDMANAGER_H
 
-//$Id: CDManager.h,v 1.39 2005/09/05 04:08:46 markus Rel $
+//$Id: CDManager.h,v 1.40 2005/10/04 16:22:21 markus Exp $
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -38,6 +38,7 @@
 #include "SongList.h"
 #include "Director.h"
 #include "Interpret.h"
+#include "ActorList.h"
 #include "MovieList.h"
 #include "RecordList.h"
 
@@ -76,6 +77,14 @@ class CDManager : public XGP::XApplication {
    void newSong ();
    void newDirector ();
    void newMovie ();
+   void newActor ();
+
+   void undo ();
+   void undoMovie (const HEntity& obj);
+   void undoSong (const HEntity& obj);
+   void undoRecord (const HEntity& obj);
+   void undoCelebrity (const HEntity& obj);
+
    void deleteSelection ();
    void editPreferences ();
    void savePreferences ();
@@ -109,6 +118,7 @@ class CDManager : public XGP::XApplication {
    Gtk::TreeIter addArtist (const HInterpret& artist);
    Gtk::TreeIter addRecord (Gtk::TreeIter& parent, HRecord& record);
    Gtk::TreeIter addSong (HSong& song);
+   Gtk::TreeIter addActor (const HActor& actor);
 
    void artistChanged (const HInterpret& artist);
    void recordChanged (const HEntity& record);
@@ -117,6 +127,7 @@ class CDManager : public XGP::XApplication {
    void directorChanged (const HDirector& director);
    void movieChanged (const HEntity& movie);
    void movieNameChanged (const HMovie& movie);
+   void actorChanged (const HActor& actor);
 
    void deleteRecord (const Gtk::TreeIter& record);
    void deleteMovie (const Gtk::TreeIter& movie);
@@ -148,23 +159,26 @@ class CDManager : public XGP::XApplication {
    Genres movieGenres;
 
    YGP::Relation1_N<HDirector, HMovie> relMovies;
+   YGP::RelationN_M<HMovie, HActor>    relActors;
 
    YGP::Relation1_N<HInterpret, HRecord> relRecords;
    YGP::Relation1_N<HRecord, HSong>      relSongs;
 
    Gtk::Notebook  nb;
 
+   RecordList records;
    SongList   songs;
    MovieList  movies;
-   RecordList records;
+   ActorList  actors;
 
    Gtk::Statusbar status;
 
-   std::vector<HSong>      deletedSongs;
-   std::vector<HRecord>    deletedRecords;
    std::vector<HInterpret> deletedInterprets;
-   std::vector<HMovie>     deletedMovies;
    std::vector<HDirector>  deletedDirectors;
+
+   std::map<HSong, HRecord>      deletedSongs;
+   std::map<HRecord, HInterpret> deletedRecords;
+   std::map<HMovie, HDirector>   deletedMovies;
 
    std::map<HSong, HSong>           changedSongs;
    std::map<HRecord, HRecord>       changedRecords;
@@ -172,6 +186,7 @@ class CDManager : public XGP::XApplication {
    std::map<HMovie, HMovie>         changedMovies;
    std::map<HMovie, std::string>    changedMovieNames;
    std::map<HDirector, HDirector>   changedDirectors;
+   std::map<HActor, HActor>         changedActors;
 
    unsigned int                loadedPages;
    std::map<std::string, bool> loadedLangs;
@@ -187,9 +202,12 @@ class CDManager : public XGP::XApplication {
 
    std::vector<HDirector>  directors;
    std::vector<HInterpret> artists;
+   std::vector<HActor>     aActors;
+
+   std::vector<HEntity> undoEntities;
 
    enum { LOGIN = 0, SAVE, LOGOUT, EXPORT, IMPORT_MP3, MEDIT, NEW1, NEW2, NEW3,
-	  DELETE, SAVE_PREFS, LAST };
+	  UNDO, DELETE, SAVE_PREFS, LAST };
    Glib::RefPtr<Gtk::Action> apMenus[LAST];
    Options& opt;
 };

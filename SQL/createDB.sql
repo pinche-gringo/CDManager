66f0b1fd93686a966723657f05d7f9ae3fddc53d
Markus Schwab <g17m0@users.sourceforge.net>
2005-11-12 13:42:23 +0000

    Don't drop tables before creating

--- SQL/createDB.sql
+++ SQL/createDB.sql
@@ -1,6 +1,5 @@
 USE CDMedia;
 
-DROP TABLE Celebrities;
 CREATE TABLE Celebrities
     (name       VARCHAR (64)    NOT NULL,
      id	        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -9,12 +8,10 @@ CREATE TABLE Celebrities
      PRIMARY KEY (id));
 CREATE INDEX names ON Celebrities (name);
 
-DROP TABLE Directors;
 CREATE TABLE Directors
     (id	        BIGINT UNSIGNED NOT NULL REFERENCES Celebrities(id),
      PRIMARY KEY (id));
 
-DROP TABLE Movies;
 CREATE TABLE Movies
     (name       VARCHAR (64)    NOT NULL,
      id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -27,7 +24,6 @@ CREATE TABLE Movies
      PRIMARY KEY (id));
 CREATE INDEX movieNames ON Movies (name);
 
-DROP TABLE MovieNames;
 CREATE TABLE MovieNames
     (id         BIGINT UNSIGNED NOT NULL REFERENCES Movies(id),
      name       VARCHAR (64)    NOT NULL,
@@ -35,22 +31,18 @@ CREATE TABLE MovieNames
      PRIMARY KEY (id, language));
 CREATE INDEX movieTranlations ON MovieNames (id);
 
-DROP TABLE Actors;
 CREATE TABLE Actors
     (id	        BIGINT UNSIGNED NOT NULL REFERENCES Celebrities(id),
      PRIMARY KEY (id));
 
-DROP TABLE ActorsInMovies;
 CREATE TABLE ActorsInMovies
     (idActor    BIGINT UNSIGNED NOT NULL REFERENCES Celebrities(id),
      idMovie    BIGINT UNSIGNED NOT NULL REFERENCES Movies(id));
 
-DROP TABLE Interprets;
 CREATE TABLE Interprets
     (id	        BIGINT UNSIGNED NOT NULL REFERENCES Celebrities(id),
      PRIMARY KEY (id));
 
-DROP TABLE Records;
 CREATE TABLE Records
     (name       VARCHAR(64)     NOT NULL,
      id	        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -61,7 +53,6 @@ CREATE TABLE Records
 CREATE INDEX recordNames ON Records (name);
 CREATE INDEX recordInterprets ON Records (interpret);
 
-DROP TABLE Songs;
 CREATE TABLE Songs
     (name       VARCHAR(64)       NOT NULL,
      id	        BIGINT UNSIGNED	  NOT NULL AUTO_INCREMENT,
@@ -73,12 +64,10 @@ CREATE TABLE Songs
 CREATE INDEX songNames ON Songs (name);
 CREATE INDEX songRecord ON Songs (idRecord);
 
-DROP TABLE Words;
 CREATE TABLE Words
     (word       VARCHAR(32)     NOT NULL,
      PRIMARY KEY (word));
 
-DROP TABLE Articles;
 CREATE TABLE Articles
     (article     VARCHAR(9)    NOT NULL,
      PRIMARY KEY (article));

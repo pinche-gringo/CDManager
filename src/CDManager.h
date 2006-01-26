da14cdbef89681652ce6af70b19d5a22aad7d8b5
Markus Schwab <g17m0@users.sourceforge.net>
2006-01-26 21:18:21 +0000

    Moved stuff to page-specific classes

--- src/CDManager.h
+++ src/CDManager.h
@@ -1,7 +1,7 @@
 #ifndef CDMANAGER_H
 #define CDMANAGER_H
 
-//$Id: CDManager.h,v 1.41 2005/10/27 21:52:34 markus Rel $
+//$Id: CDManager.h,v 1.42 2006/01/26 21:18:21 markus Exp $
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,26 +18,26 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 
+#include <cdmgr-cfg.h>
+
+#if !defined (WITH_ACTORS) || !defined (WITH_RECORDS) || !defined (WITH_MOVIES)
+#  error Need WITH_ACTORS, WITH_RECORDS and WITH_MOVIES defined
+#endif
+
 #include <map>
 #include <vector>
 
 #include <gtkmm/table.h>
-#include <gtkmm/paned.h>
 #include <gtkmm/notebook.h>
 #include <gtkmm/treeview.h>
 #include <gtkmm/statusbar.h>
 
 #include "Song.h"
-#include "Movie.h"
 #include "Genres.h"
 #include "Record.h"
 #include "Options.h"
-#include "LangImg.h"
 #include "SongList.h"
-#include "Director.h"
 #include "Interpret.h"
-#include "ActorList.h"
-#include "MovieList.h"
 #include "RecordList.h"
 
 #include <YGP/Relation.h>
@@ -46,6 +46,7 @@
 
 
 // Forward declarations
+class NBPage;
 namespace YGP {
    class Entity;
    class StatusObject;
@@ -69,85 +70,26 @@ class CDManager : public XGP::XApplication {
    void save ();
    void showLogin ();
    void logout ();
-   void exportToHTML ();
    void importFromFileInfo ();
-   void newInterpret ();
-   void newRecord ();
-   void newSong ();
-   void newDirector ();
-   void newMovie ();
-   void newActor ();
-   void actorPlaysInMovie ();
-   void relateMovies (const HActor& actor, const std::vector<HMovie>& movies);
+#if (WITH_RECORDS == 1) || (WITH_MOVIES == 1)
+   void export2HTML ();
+#endif
 
-   void undo ();
-   void undoMovie (const HEntity& obj);
-   void undoSong (const HEntity& obj);
-   void undoRecord (const HEntity& obj);
-   void undoCelebrity (const HEntity& obj);
-
-   void deleteSelection ();
    void editPreferences ();
    void savePreferences ();
-   void storeWord (const char* word);
-   void storeArticle (const char* article);
-
-   void selectLanguage ();
-   void setLanguage (const std::string& lang);
-   void changeLanguage (const std::string& lang);
 
    virtual void showAboutbox ();
    virtual const char* getHelpfile ();
-   void recordSelected ();
-   void movieSelected ();
-   void actorSelected ();
    void pageSwitched (GtkNotebookPage* page, guint iPage);
-
-   typedef enum { NONE_SELECTED, OWNER_SELECTED, OBJECT_SELECTED } SELECTED;
-   void enableEdit (SELECTED selected);
 
    bool login (const Glib::ustring& user, const Glib::ustring& pwd);
    void loadDatabase ();
-   void loadRecords ();
-   void loadMovies ();
-   void loadMovies (const std::string& lang);
-   void loadActors ();
-   void loadCelebrities (std::vector<HCelebrity>& target, const std::string& table,
-			 YGP::StatusObject& stat);
 
-   HMovie findMovie (unsigned int id) const;
-   void addLanguageMenus (Glib::ustring& menu, Glib::RefPtr<Gtk::ActionGroup> grpAction);
    void enableMenus (bool enable);
-   void loadSongs (const HRecord& record);
-   void exportData () throw (Glib::ustring);
    void exit ();
    bool on_delete_event (GdkEventAny*);
 
-   Gtk::TreeIter addArtist (const HInterpret& artist);
-   Gtk::TreeIter addRecord (Gtk::TreeIter& parent, HRecord& record);
-   Gtk::TreeIter addSong (HSong& song);
-   Gtk::TreeIter addActor (const HActor& actor);
-
-   void artistChanged (const HInterpret& artist);
-   void recordChanged (const HEntity& record);
-   void recordGenreChanged (const HEntity& record);
-   void songChanged (const HSong& song);
-   void directorChanged (const HDirector& director);
-   void movieChanged (const HEntity& movie);
-   void movieNameChanged (const HMovie& movie);
-   void actorChanged (const HActor& actor);
-
-   void deleteRecord (const Gtk::TreeIter& record);
-   void deleteMovie (const Gtk::TreeIter& movie);
-   void deleteSelectedRecords ();
-   void deleteSelectedSongs ();
-   void deleteSelectedMovies ();
-   void deleteSelectedActor ();
-
-   void removeDeletedEntries ();
-   void writeChangedEntries ();
-   static Glib::ustring escapeDBValue (const Glib::ustring& value);
-
+#if WITH_RECORDS == 1
    std::string stripString (const std::string& value, unsigned int pos, unsigned int len);
    void parseFileInfo (const std::string& file);
    bool parseMP3Info (std::istream& stream, Glib::ustring& artist,
@@ -155,6 +97,7 @@ class CDManager : public XGP::XApplication {
    bool parseOGGCommentHeader (std::istream& stream, Glib::ustring& artist,
 			       Glib::ustring& record, Glib::ustring& song,
 			       unsigned int& track);
+#endif
 
    static const char* xpmProgram[];
    static const char* xpmAuthor[];
@@ -167,62 +110,21 @@ class CDManager : public XGP::XApplication {
    Genres recGenres;
    Genres movieGenres;
 
-   YGP::Relation1_N<HDirector, HMovie> relMovies;
-   YGP::RelationN_M<HActor, HMovie>    relActors;
-   YGP::RelationN_M<HActor, HMovie>    relDelActors;
-
-   YGP::Relation1_N<HInterpret, HRecord> relRecords;
-   YGP::Relation1_N<HRecord, HSong>      relSongs;
-
    Gtk::Notebook  nb;
-
-   RecordList records;
-   SongList   songs;
-   MovieList  movies;
-   ActorList  actors;
-
    Gtk::Statusbar status;
 
-   std::vector<HInterpret> deletedInterprets;
-   std::vector<HDirector>  deletedDirectors;
-   std::vector<HActor>     deletedActors;
-
-   std::map<HSong, HRecord>      deletedSongs;
-   std::map<HRecord, HInterpret> deletedRecords;
-   std::map<HMovie, HDirector>   deletedMovies;
-
-   std::map<HSong, HSong>           changedSongs;
-   std::map<HRecord, HRecord>       changedRecords;
-   std::map<HInterpret, HInterpret> changedInterprets;
-   std::map<HMovie, HMovie>         changedMovies;
-   std::map<HMovie, std::string>    changedMovieNames;
-   std::map<HDirector, HDirector>   changedDirectors;
-   std::map<HActor, HActor>         changedActors;
-
-   std::vector<HActor> changedRelMovies;
-
-   unsigned int                loadedPages;
-   std::map<std::string, bool> loadedLangs;
-
-   union PageData {
-      LanguageImg* img;
-
-      PageData () { img = NULL; }
-      ~PageData () { clean () ;}
-
-      void clean () { delete img; img = NULL; }
-   } pageData;
-
-   std::vector<HDirector>  directors;
-   std::vector<HInterpret> artists;
-   std::vector<HActor>     aActors;
-
-   std::vector<HEntity> undoEntities;
-
-   enum { LOGIN = 0, SAVE, LOGOUT, EXPORT, IMPORT_MP3, MEDIT, NEW1, NEW2, NEW3,
-	  UNDO, DELETE, SAVE_PREFS, LAST };
+   enum { LOGIN = 0, SAVE, LOGOUT, MEDIT, SAVE_PREFS,
+#if (WITH_RECORDS == 1) || (WITH_MOVIES == 1)
+	  EXPORT,
+#endif
+#if WITH_RECORDS == 1
+	  IMPORT_MP3,
+	  LAST };
+#endif
    Glib::RefPtr<Gtk::Action> apMenus[LAST];
    Options& opt;
+
+   NBPage* pages[WITH_ACTORS + WITH_MOVIES + WITH_RECORDS];
 };
 
 #endif

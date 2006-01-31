d1bad98e5a3b643dc6d5c1a5c52ef84079d7d0ba
Markus Schwab <g17m0@users.sourceforge.net>
2006-01-31 20:46:48 +0000

    Added entries for undo

--- src/NBPage.h
+++ src/NBPage.h
@@ -1,7 +1,7 @@
 #ifndef NBPAGE_H
 #define NBPAGE_H
 
-//$Id: NBPage.h,v 1.2 2006/01/28 01:16:17 markus Exp $
+//$Id: NBPage.h,v 1.3 2006/01/31 20:46:48 markus Exp $
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,11 +18,17 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 
+#include <map>
+#include <stack>
+#include <vector>
+
 #include <glibmm/ustring.h>
 
 #include <gtkmm/action.h>
+#include <gtkmm/treepath.h>
 #include <gtkmm/actiongroup.h>
-#include <gtkmm/box.h>
+
+#include <YGP/Entity.h>
 
 #include <sigc++/trackable.h>
 
@@ -72,7 +78,39 @@ class NBPage : public sigc::trackable {
    enum { NEW1, NEW2, NEW3, UNDO, DELETE, LAST };
    Glib::RefPtr<Gtk::Action> apMenus[LAST];
 
+   /**Undo-info
+    */
+   class Undo {
+   public:
+      typedef enum { UNDEFINED = 0, INSERT, DELETE, CHANGED } CHGSPEC;
+      Undo () { chgSpec.how = UNDEFINED; };
+      Undo (CHGSPEC chg, unsigned int what, unsigned int col, Gtk::TreePath& row,
+	    const Glib::ustring& value) : row (row), value (value) {
+	 chgSpec.how = chg;
+	 chgSpec.what = what;
+	 chgSpec.column = col;
+       }
+
+      unsigned int how () const { return chgSpec.how; }
+      unsigned int what () const { return chgSpec.what; }
+      unsigned int column () const { return chgSpec.column; }
+      const Gtk::TreePath& getPath () const { return row; }
+      const Glib::ustring& getValue () const { return value; }
+
+   private:
+      struct {
+	 unsigned int column : 16;
+	 unsigned int how    : 2;
+	 unsigned int what   : 3;
+      } chgSpec;
+      Gtk::TreePath row;
+      Glib::ustring value;
+   };
+
    bool loaded;
+   std::stack<Undo>                     aUndo;
+   std::vector<YGP::HEntity>            delEntries;
+   std::map<YGP::HEntity, YGP::HEntity> delRelation;
 
  private:
    NBPage (const NBPage&);
